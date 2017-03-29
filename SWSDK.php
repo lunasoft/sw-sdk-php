41f49fda1190e760a78e5a06c8947f297f3b18a9
Heltton <heltton.ramirez@sw.com.mx>
2017-03-29 14:47:19 -0600

    SDT-294 cambio de ruta para que encuentre los archivos las pruebas unitarias en trevis ci.

--- SWSDK.php
+++ SWSDK.php
@@ -1,10 +1,10 @@
 <?php
     require "vendor/autoload.php";
-    require_once dirname(__FILE__) . '\SWServices\Services.php';
-    require_once dirname(__FILE__) . '\SWServices\Authentication\AuthenticationService.php';
-    require_once dirname(__FILE__) . '\SWServices\Authentication\AuthRequest.php';
-    require_once dirname(__FILE__) . '\SWServices\Stamp\StampRequest.php';
-    require_once dirname(__FILE__) . '\SWServices\Stamp\StampService.php';
+    require_once dirname(__FILE__) . '/SWServices/Services.php';
+    require_once dirname(__FILE__) . '/SWServices/Authentication/AuthenticationService.php';
+    require_once dirname(__FILE__) . '/SWServices/Authentication/AuthRequest.php';
+    require_once dirname(__FILE__) . '/SWServices/Stamp/StampRequest.php';
+    require_once dirname(__FILE__) . '/SWServices/Stamp/StampService.php';
 
 ?>
    
