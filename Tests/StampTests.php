e31abc8fdf48d026bc348921cba94de636c4945f
Rich Barusta <ricardo.barusta@sw.com.mx>
2017-05-12 15:05:10 -0500

    SDT-218

--- Tests/StampTests.php
+++ Tests/StampTests.php
@@ -8,9 +8,14 @@
     
 	final class StampTests extends TestCase{
 		
-
+		
+/*----------------------------------------V1---------------------------------------------------------------------------------------------------------------*/
         public function testStampXMLV1()
         {
+			function is_base64($s)
+			{
+				return (bool) preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s);
+			}
 			$resultSpect = "success";
 			$params = array(
 			    "url"=>"http://services.test.sw.com.mx",
@@ -54,9 +59,13 @@
 			$stamp = StampService::Set($params);
 			$result = $stamp::StampV2($xml);
 			$result = json_decode($result);
+			var_dump("---------------------------TFD---------------------------------------------------------------------------------------------------------------------------");
+			var_dump($result->data->tfd);
+			var_dump("---------------------------CFDI---------------------------------------------------------------------------------------------------------------------------");
+			var_dump($result->data->cfdi);
 			$this->assertEquals($resultSpect,$result->status);
         }
-
+/*----------------------------------V2---------------------------------------------------------------------------------------------------------------------*/
         public function testStampXMLV2byToken()
         {
 			$resultSpect = "success";
@@ -69,7 +78,170 @@
 			$stamp = StampService::Set($params);
 			$result = $stamp::StampV2($xml);
 			$result = json_decode($result);
+			var_dump($result);
 			$this->assertEquals($resultSpect,$result->status);
+        }
+
+		 public function testStampXMLV2_B64()
+        {
+			
+			$resultSpect = "success";
+			$params = array(
+			    "url"=>"http://swservicestest-rc.azurewebsites.net",
+			    "user"=>"demo",
+			    "password"=> "12345678A"
+				);
+			//$xml = $GLOBALS["xml"];
+			$xml = file_get_contents('./Tests/Resources/B64.txt');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV2($xml,true);
+			$result = json_decode($result);
+			var_dump($result);
+			$is_valid_b64_tfd = is_base64($result->data->tfd);
+			$is_valid_b64_cfdi = is_base64($result->data->cfdi);
+			
+			
+			$this->assertTrue($is_valid_b64_cfdi && $is_valid_b64_tfd);
+        }
+
+        public function testStampXMLV2byToken_B64()
+        {
+			$resultSpect = "success";
+			$params = array(
+				"url"=>"http://swservicestest-rc.azurewebsites.net",
+				"token"=>"T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRiMTFPRlV3a2kyOWI5WUZHWk85ODJtU0M2UlJEUkFTVXhYTDNKZVdhOXIySE1tUVlFdm1jN3kvRStBQlpLRi9NeWJrd0R3clhpYWJrVUMwV0Mwd3FhUXdpUFF5NW5PN3J5cklMb0FETHlxVFRtRW16UW5ZVjAwUjdCa2g0Yk1iTExCeXJkVDRhMGMxOUZ1YWlIUWRRVC8yalFTNUczZXdvWlF0cSt2UW0waFZKY2gyaW5jeElydXN3clNPUDNvU1J2dm9weHBTSlZYNU9aaGsvalpQMUxrUndzK0dHS2dpTittY1JmR3o2M3NqNkh4MW9KVXMvUHhZYzVLQS9UK2E1SVhEZFJKYWx4ZmlEWDFuSXlqc2ZRYXlUQk1ldlZkU2tEdU10NFVMdHZKUURLblBxakw0SDl5bUxabDFLNmNPbEp6b3Jtd2Q1V2htRHlTdDZ6eTFRdUNnYnVvK2tuVUdhMmwrVWRCZi9rQkU9.7k2gVCGSZKLzJK5Ky3Nr5tKxvGSJhL13Q8W-YhT0uIo"
+				);
+			$xml = file_get_contents('./Tests/Resources/b64.txt');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV2($xml,true);
+			$result = json_decode($result);
+			var_dump($result);
+			$is_valid_b64_tfd = is_base64($result->data->tfd);
+			$is_valid_b64_cfdi = is_base64($result->data->cfdi);
+			
+			
+			$this->assertTrue($is_valid_b64_cfdi && $is_valid_b64_tfd);
+        }
+
+		/*--------------------------------V3-----------------------------------------------------------------------------------------------------------------------*/
+		 public function testStampXMLV3byToken()
+        {
+			$resultSpect = "success";
+			$params = array(
+				"url"=>"http://swservicestest-rc.azurewebsites.net",
+				"token"=>"T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRiMTFPRlV3a2kyOWI5WUZHWk85ODJtU0M2UlJEUkFTVXhYTDNKZVdhOXIySE1tUVlFdm1jN3kvRStBQlpLRi9NeWJrd0R3clhpYWJrVUMwV0Mwd3FhUXdpUFF5NW5PN3J5cklMb0FETHlxVFRtRW16UW5ZVjAwUjdCa2g0Yk1iTExCeXJkVDRhMGMxOUZ1YWlIUWRRVC8yalFTNUczZXdvWlF0cSt2UW0waFZKY2gyaW5jeElydXN3clNPUDNvU1J2dm9weHBTSlZYNU9aaGsvalpQMUxrUndzK0dHS2dpTittY1JmR3o2M3NqNkh4MW9KVXMvUHhZYzVLQS9UK2E1SVhEZFJKYWx4ZmlEWDFuSXlqc2ZRYXlUQk1ldlZkU2tEdU10NFVMdHZKUURLblBxakw0SDl5bUxabDFLNmNPbEp6b3Jtd2Q1V2htRHlTdDZ6eTFRdUNnYnVvK2tuVUdhMmwrVWRCZi9rQkU9.7k2gVCGSZKLzJK5Ky3Nr5tKxvGSJhL13Q8W-YhT0uIo"
+				);
+			$xml = file_get_contents('./Tests/Resources/file.xml');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV3($xml);
+			$result = json_decode($result);
+			var_dump($result);
+			$this->assertEquals($resultSpect,$result->status);
+        }
+
+		 public function testStampXMLV3_B64()
+        {
+			
+			$resultSpect = "success";
+			$params = array(
+			    "url"=>"http://swservicestest-rc.azurewebsites.net",
+			    "user"=>"demo",
+			    "password"=> "12345678A"
+				);
+			//$xml = $GLOBALS["xml"];
+			$xml = file_get_contents('./Tests/Resources/B64.txt');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV3($xml,true);
+			$result = json_decode($result);
+			var_dump($result);
+			
+			$is_valid_b64_cfdi = is_base64($result->data->cfdi);
+			
+			
+			$this->assertTrue($is_valid_b64_cfdi);
+        }
+
+        public function testStampXMLV3byToken_B64()
+        {
+			$resultSpect = "success";
+			$params = array(
+				"url"=>"http://swservicestest-rc.azurewebsites.net",
+				"token"=>"T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRiMTFPRlV3a2kyOWI5WUZHWk85ODJtU0M2UlJEUkFTVXhYTDNKZVdhOXIySE1tUVlFdm1jN3kvRStBQlpLRi9NeWJrd0R3clhpYWJrVUMwV0Mwd3FhUXdpUFF5NW5PN3J5cklMb0FETHlxVFRtRW16UW5ZVjAwUjdCa2g0Yk1iTExCeXJkVDRhMGMxOUZ1YWlIUWRRVC8yalFTNUczZXdvWlF0cSt2UW0waFZKY2gyaW5jeElydXN3clNPUDNvU1J2dm9weHBTSlZYNU9aaGsvalpQMUxrUndzK0dHS2dpTittY1JmR3o2M3NqNkh4MW9KVXMvUHhZYzVLQS9UK2E1SVhEZFJKYWx4ZmlEWDFuSXlqc2ZRYXlUQk1ldlZkU2tEdU10NFVMdHZKUURLblBxakw0SDl5bUxabDFLNmNPbEp6b3Jtd2Q1V2htRHlTdDZ6eTFRdUNnYnVvK2tuVUdhMmwrVWRCZi9rQkU9.7k2gVCGSZKLzJK5Ky3Nr5tKxvGSJhL13Q8W-YhT0uIo"
+				);
+			$xml = file_get_contents('./Tests/Resources/b64.txt');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV3($xml,true);
+			$result = json_decode($result);
+			var_dump($result);
+			
+			$is_valid_b64_cfdi = is_base64($result->data->cfdi);
+			
+			
+			$this->assertTrue($is_valid_b64_cfdi);
+        }
+		/*--------------------------------V4-----------------------------------------------------------------------------------------------------------------------*/
+		 public function testStampXMLV4byToken()
+        {
+			$resultSpect = "success";
+			$params = array(
+				"url"=>"http://swservicestest-rc.azurewebsites.net",
+				"token"=>"T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRiMTFPRlV3a2kyOWI5WUZHWk85ODJtU0M2UlJEUkFTVXhYTDNKZVdhOXIySE1tUVlFdm1jN3kvRStBQlpLRi9NeWJrd0R3clhpYWJrVUMwV0Mwd3FhUXdpUFF5NW5PN3J5cklMb0FETHlxVFRtRW16UW5ZVjAwUjdCa2g0Yk1iTExCeXJkVDRhMGMxOUZ1YWlIUWRRVC8yalFTNUczZXdvWlF0cSt2UW0waFZKY2gyaW5jeElydXN3clNPUDNvU1J2dm9weHBTSlZYNU9aaGsvalpQMUxrUndzK0dHS2dpTittY1JmR3o2M3NqNkh4MW9KVXMvUHhZYzVLQS9UK2E1SVhEZFJKYWx4ZmlEWDFuSXlqc2ZRYXlUQk1ldlZkU2tEdU10NFVMdHZKUURLblBxakw0SDl5bUxabDFLNmNPbEp6b3Jtd2Q1V2htRHlTdDZ6eTFRdUNnYnVvK2tuVUdhMmwrVWRCZi9rQkU9.7k2gVCGSZKLzJK5Ky3Nr5tKxvGSJhL13Q8W-YhT0uIo"
+				);
+			$xml = file_get_contents('./Tests/Resources/file.xml');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV4($xml);
+			$result = json_decode($result);
+			var_dump($result);
+			$this->assertEquals($resultSpect,$result->status);
+        }
+
+		 public function testStampXMLV4_B64()
+        {
+			
+			$resultSpect = "success";
+			$params = array(
+			    "url"=>"http://swservicestest-rc.azurewebsites.net",
+			    "user"=>"demo",
+			    "password"=> "12345678A"
+				);
+			//$xml = $GLOBALS["xml"];
+			$xml = file_get_contents('./Tests/Resources/B64.txt');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV4($xml,true);
+			$result = json_decode($result);
+			var_dump($result);
+			
+			$is_valid_b64_cfdi = is_base64($result->data->cfdi);
+			
+			
+			$this->assertTrue($is_valid_b64_cfdi);
+        }
+
+        public function testStampXMLV4byToken_B64()
+        {
+			$resultSpect = "success";
+			$params = array(
+				"url"=>"http://swservicestest-rc.azurewebsites.net",
+				"token"=>"T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRiMTFPRlV3a2kyOWI5WUZHWk85ODJtU0M2UlJEUkFTVXhYTDNKZVdhOXIySE1tUVlFdm1jN3kvRStBQlpLRi9NeWJrd0R3clhpYWJrVUMwV0Mwd3FhUXdpUFF5NW5PN3J5cklMb0FETHlxVFRtRW16UW5ZVjAwUjdCa2g0Yk1iTExCeXJkVDRhMGMxOUZ1YWlIUWRRVC8yalFTNUczZXdvWlF0cSt2UW0waFZKY2gyaW5jeElydXN3clNPUDNvU1J2dm9weHBTSlZYNU9aaGsvalpQMUxrUndzK0dHS2dpTittY1JmR3o2M3NqNkh4MW9KVXMvUHhZYzVLQS9UK2E1SVhEZFJKYWx4ZmlEWDFuSXlqc2ZRYXlUQk1ldlZkU2tEdU10NFVMdHZKUURLblBxakw0SDl5bUxabDFLNmNPbEp6b3Jtd2Q1V2htRHlTdDZ6eTFRdUNnYnVvK2tuVUdhMmwrVWRCZi9rQkU9.7k2gVCGSZKLzJK5Ky3Nr5tKxvGSJhL13Q8W-YhT0uIo"
+				);
+			$xml = file_get_contents('./Tests/Resources/b64.txt');
+			
+			$stamp = StampService::Set($params);
+			$result = $stamp::StampV4($xml,true);
+			$result = json_decode($result);
+			var_dump($result);
+			
+			$is_valid_b64_cfdi = is_base64($result->data->cfdi);
+			
+			
+			$this->assertTrue($is_valid_b64_cfdi);
         }
     }
 
