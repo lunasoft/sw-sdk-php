4fe8fa3b3b5251f775f92f1ad88178c6913e9f36
Emmanuel <emmanuel.contreras@sw.com.mx>
2018-07-12 18:29:00 -0500

    no message

--- tests/AuthTests.php
+++ tests/AuthTests.php
@@ -1,20 +1,22 @@
 <?php
     namespace tests;
-
+    
+    use PHPUnit\Framework\TestCase;
     use SWServices\Authentication\AuthenticationService as AuthenticationService;
     use Exception;
 
-    final class AuthTests {
+    final class AuthTests extends TestCase{
         public function testSuccess(){
             $params = array(
                 "url"=>"http://services.test.sw.com.mx",
                 "user"=>"demo",
                 "password"=> "123456789"
             );
-             $this->assertInstanceOf(
-                    AuthenticationService::class,
-                    AuthenticationService::auth($params)
-                );
+            $authenticate = AuthenticationService::auth($params);
+            $result = $authenticate::Token();
+            $result->status;
+                  
+            $this->assert($result->status, "success");
         }
         public function testError(){
             $this->expectException(Exception::class);
