89127f88e5b2227ed3b22373b17dd8d76a8faea2
Reyes (David Ernesto) <105820244+ReyesSW@users.noreply.github.com>
2022-12-09 14:32:33 -0600

    Se agrega documentación para los metodos publicos.

--- SWServices/Issuer/IssuerService.php
+++ SWServices/Issuer/IssuerService.php
@@ -41,6 +41,17 @@ class EmisionTimbrado extends Services{
     }
     //Timbrado V4 Issue 
 
+    /**
+     * Servicio que consume el timbrado V4 que envia un customId, tambien que envia un correo con el XML y el pdf adjunto 
+     * @param xml $xml documento a timbrar.
+     * @param customId $customId dato para determinar dato unico.
+     * @param email $email Correo o correos electronicos al que se enviara el resultado de la peticion, maximo 5 correos.
+     * @param pdf $pdf variable que determina si se genera y adjunta el pdf en la peticion.
+     * @return stampRequest
+     */
+    //Instanciar StampRequest
+
+    //Funcion que recibe el CustomId y timbra el XML con version V1 de respuesta
     public static function stampV4CustomIdPdfV1($xml, $customId, $pdf = false ,$isb64 = false){
         $params = array(
             "xml" => $xml,
@@ -49,6 +60,7 @@ class EmisionTimbrado extends Services{
         $helper = new StampHelper($params);
         return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v1", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
+    //Funcion que recibe el CustomId y timbra el XML con version V2 de respuesta
     public static function stampV4CustomIdPdfV2($xml, $customId, $pdf = false, $isb64 = false){
         $params = array(
             "xml" => $xml,
@@ -57,6 +69,7 @@ class EmisionTimbrado extends Services{
         $helper = new StampHelper($params);
         return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v2", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
+    //Funcion que recibe el CustomId y timbra el XML con version V3 de respuesta
     public static function stampV4CustomIdPdfV3($xml, $customId, $pdf = false, $isb64 = false){
         $params = array(
             "xml" => $xml,
@@ -65,6 +78,7 @@ class EmisionTimbrado extends Services{
         $helper = new StampHelper($params);
         return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v3", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
+    //Funcion que recibe el CustomId y timbra el XML con version V4 de respuesta
     public static function stampV4CustomIdPdfV4($xml, $customId, $pdf = false, $isb64 = false){
         $params = array(
             "xml" => $xml,
@@ -73,6 +87,7 @@ class EmisionTimbrado extends Services{
         $helper = new StampHelper($params);
         return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v4", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
+    //Funcion que recibe el CustomId, envia correo, genera pdf y timbra el XML con version V1 de respuesta
     public static function stampV4CustomIdEmailV1($xml, $customId, $email, $isb64 = false){
         $params = array(
             "xml" => $xml,
@@ -82,6 +97,7 @@ class EmisionTimbrado extends Services{
         $helper = new StampHelper($params);
         return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v1", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), true, $helper::validate_email($email));
     }
+    //Funcion que recibe el CustomId, envia correo, genera pdf y timbra el XML con version V2 de respuesta
     public static function stampV4CustomIdEmailV2($xml, $customId, $email, $isb64 = false){
         $params = array(
             "xml" => $xml,
@@ -91,6 +107,7 @@ class EmisionTimbrado extends Services{
         $helper = new StampHelper($params);
         return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v2", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), true, $helper::validate_email($email));
     }
+    //Funcion que recibe el CustomId, envia correo, genera pdf y timbra el XML con version V3 de respuesta
     public static function stampV4CustomIdEmailV3($xml, $customId, $email, $isb64 = false){
         $params = array(
             "xml" => $xml,
@@ -100,6 +117,7 @@ class EmisionTimbrado extends Services{
         $helper = new StampHelper($params);
         return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v3", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), true, $helper::validate_email($email));
     }
+    //Funcion que recibe el CustomId, envia correo, genera pdf y timbra el XML con version V4 de respuesta
     public static function stampV4CustomIdEmailV4($xml, $customId, $email, $isb64 = false){
         $params = array(
             "xml" => $xml,
