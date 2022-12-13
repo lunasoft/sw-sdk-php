e397a91673c0b834fb5af5e34a9fc8cf19fd0900
Reyes (David Ernesto) <105820244+ReyesSW@users.noreply.github.com>
2022-12-13 13:05:32 -0600

    Se realiza un cambio en la phpDoc.

--- SWServices/Issuer/IssuerService.php
+++ SWServices/Issuer/IssuerService.php
@@ -54,58 +54,148 @@ class EmisionTimbrado extends Services
     //Timbrado V4 Issue 
 
     /**
-     *  * @autor David Ernesto Reyes <david.reyes@sw.com.mx>
-     *
-     * @desde 1.0.7.1
-     *
-     * Servicio que consume el timbrado V4 que envia un customId, tambien que envia un correo con el XML y el pdf adjunto
-     * El customId es un identificador unico que el cliente determina, al utilizar el envio de correo por default se genera
-     * un pdf que se adjunta al mismo cuerpo del correo junto con el XML
      * 
+     * Servicio que consume el timbrado V4 que envia un customId y regresa con la version V1 de respuesta que regresa el TFD(Timbre Fiscal Digital)
      * 
-     * 
-     * @param xml $xml documento a timbrar.
-     * @param customId $customId dato para determinar dato unico.
-     * @param email $email Correo o correos electronicos al que se enviara el resultado de la peticion, maximo 5 correos en un arreglo.
-     * @param pdf $pdf variable que determina si se genera y adjunta el pdf en la peticion.
+     * @param string xml $xml documento a timbrar.
+     * @param string customId $customId dato para determinar dato unico.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion, dentro de esta funcion esta false por defecto
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
      * 
      * @return stampRequest
      * 
      */
-    //Instanciar StampRequest
-
-
     public static function stampV4CustomIdPdfV1($xml, $customId, $pdf = false, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v1", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), $pdf, NULL);
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v1", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
+    /**
+     * 
+     * Servicio que consume el timbrado V4 que envia un customId y regresa con la version V2 de respuesta que regresa el TFD(Timbre Fiscal Digital) y el XML timbrado.
+     * 
+     * @param string xml $xml documento a timbrar.
+     * @param string customId $customId dato para determinar dato unico.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion, dentro de esta funcion esta false por defecto
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
+     * 
+     * @return stampRequest
+     * 
+     */
     public static function stampV4CustomIdPdfV2($xml, $customId, $pdf = false, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v2", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), $pdf, NULL);
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v2", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
+    /**
+     * 
+     * Servicio que consume el timbrado V4 que envia un customId y regresa con la version V3 de respuesta que regresa el CFDI timbrado.
+     * 
+     * @param string xml $xml documento a timbrar.
+     * @param string customId $customId dato para determinar dato unico.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion, dentro de esta funcion esta false por defecto
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
+     * 
+     * @return stampRequest
+     * 
+     */
     public static function stampV4CustomIdPdfV3($xml, $customId, $pdf = false, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v3", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), $pdf, NULL);
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v3", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
+    /**
+     * 
+     * Servicio que consume el timbrado V4 que envia un customId y regresa con la version V4 de respuesta que regresa todos los datos del timbrado
+     * 
+     * @param string xml $xml documento a timbrar.
+     * @param string customId $customId dato para determinar dato unico.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion, dentro de esta funcion esta false por defecto
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
+     * 
+     * @return stampRequest
+     * 
+     */
     public static function stampV4CustomIdPdfV4($xml, $customId, $pdf = false, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v4", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), $pdf, NULL);
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v4", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), $pdf, NULL);
     }
 
+    /**
+     * 
+     * Servicio que consume el timbrado V4 que envia un customId, un correo electronico o un arreglo de maximo 5 correos, que genera el pdf y lo adjunta al mismo envio del correo
+     * ademas regresa con la version V1 de respuesta que regresa el TFD(Timbre Fiscal Digital)
+     * 
+     * @param string xml $xml documento a timbrar.
+     * @param string $customId dato para determinar dato unico.
+     * @param string/array $email Correo o correos electronicos al que se enviara el resultado de la peticion, maximo 5 correos en un arreglo.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion.
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
+     * 
+     * @return stampRequest
+     * 
+     */
     public static function stampV4CustomIdEmailV1($xml, $customId, $email, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v1", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), true, ($email));
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v1", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), true, $helper::validate_email($email));
     }
+    /**
+     * 
+     * Servicio que consume el timbrado V4 que envia un customId, un correo electronico o un arreglo de maximo 5 correos, que genera el pdf y lo adjunta al mismo envio del correo
+     * ademas regresa con la version V2 de respuesta que regresa el TFD(Timbre Fiscal Digital)
+     * 
+     * @param string xml $xml documento a timbrar.
+     * @param string $customId dato para determinar dato unico.
+     * @param string/array $email Correo o correos electronicos al que se enviara el resultado de la peticion, maximo 5 correos en un arreglo.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion.
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
+     * 
+     * @return stampRequest
+     * 
+     */
     public static function stampV4CustomIdEmailV2($xml, $customId, $email, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v2", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), true, ($email));
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v2", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), true, $helper::validate_email($email));
     }
+    /**
+     * 
+     * Servicio que consume el timbrado V4 que envia un customId, un correo electronico o un arreglo de maximo 5 correos, que genera el pdf y lo adjunta al mismo envio del correo
+     * ademas regresa con la version V3 de respuesta que regresa el TFD(Timbre Fiscal Digital)
+     * 
+     * @param string xml $xml documento a timbrar.
+     * @param string $customId dato para determinar dato unico.
+     * @param string/array $email Correo o correos electronicos al que se enviara el resultado de la peticion, maximo 5 correos en un arreglo.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion.
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
+     * 
+     * @return stampRequest
+     * 
+     */
     public static function stampV4CustomIdEmailV3($xml, $customId, $email, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v3", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), true, ($email));
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v3", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), true, $helper::validate_email($email));
     }
+    /**
+     * 
+     * Servicio que consume el timbrado V4 que envia un customId, un correo electronico o un arreglo de maximo 5 correos, que genera el pdf y lo adjunta al mismo envio del correo
+     * ademas regresa con la version V4 de respuesta que regresa el TFD(Timbre Fiscal Digital)
+     * 
+     * @param string xml $xml documento a timbrar.
+     * @param string $customId dato para determinar dato unico.
+     * @param string/array $email Correo o correos electronicos al que se enviara el resultado de la peticion, maximo 5 correos en un arreglo.
+     * @param bool $pdf variable que determina si se genera y adjunta el pdf en la peticion.
+     * @param bool $isb64 variable que determina si se recibe el XML en base64.
+     * 
+     * @return stampRequest
+     * 
+     */
     public static function stampV4CustomIdEmailV4($xml, $customId, $email, $isb64 = false)
     {
-        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v4", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', ($customId), true, ($email));
+        $helper = new StampHelper();
+        return stampRequest::sendReqV4(Services::get_url(), Services::get_token(), $xml, "v4", $isb64, Services::get_proxy(), '/v4/cfdi33/issue/', $helper::validate_customid($customId), true, $helper::validate_email($email));
     }
 }
