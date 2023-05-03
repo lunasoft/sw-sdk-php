cd4daf11ce92afe7ce7d3b76ea0fe32e65c4e5f8
Sebastian Vidal <sebastian.vidal@sw.com.mx>
2023-05-02 18:01:50 -0600

    Add UT Estatus
    Se agrego estado "No Encontrado" y valores a la UT

--- Test/SatQueryTests.php
+++ Test/SatQueryTests.php
@@ -7,13 +7,13 @@
 
     final class SatQueryTests extends TestCase{
         public function testSuccess(){
-            $resultSpect = ["Vigente", "Cancelado"];
+            $resultSpect = ["Vigente", "Cancelado","No Encontrado"];
             $url="https://pruebacfdiconsultaqr.cloudapp.net/ConsultaCFDIService.svc";
-            $rfcEmisor="";
-            $rfcReceptor=""; 
-            $total=""; 
-            $uuid=""; 
-            $sello="";
+            $rfcEmisor="EKU9003173C9";
+            $rfcReceptor="URE180429TM6"; 
+            $total="198.96"; 
+            $uuid="372d425a-af92-4c0e-af1e-553e7ca9946a&fe"; 
+            $sello="tBhmcQ==";
             
             $result = SatQueryService::ServicioConsultaSAT($url,$rfcEmisor, $rfcReceptor, $total, $uuid, $sello);
 			$this->assertContains($result->Status, $resultSpect);
