bd2cb88dd1b9da84919107010e8562db9f46606c
Fernando Carrillo <108951984+FerCarrilloM@users.noreply.github.com>
2024-02-16 11:58:21 -0600

    Se optimizan los métodos.

--- SWServices/AccountUser/AccountUserService.php
+++ SWServices/AccountUser/AccountUserService.php
@@ -18,20 +18,11 @@ class AccountUserService extends Services
         return new AccountUserService($params);
     }
     //Función para crear un usuario
-    public static function CreateUser($email, $password, $name, $rfc, $profile, $stamps, $unlimited, $active){
-        $data = array(
-            'Email' => $email,
-            'Pasword' => $password,
-            'Name' => $password,
-            'RFC' => $password,
-            'Profile' => $password,
-            'Stamps' => $password,
-            'Unlimited' => $unlimited,
-            'Active' => $password
-        );
+    public static function CreateUser($data){
+       return AccountUserRequest::sendReqServicesUser(null, $data, "post");
     }
     //Función para consultar usuario por token
-    public static function GetUserByToken(){
+    public static function GetUser(){
         return AccountUserRequest::sendReqGetUser();
     }
     //Función para consultar usuarios
@@ -43,13 +34,8 @@ class AccountUserService extends Services
         return AccountUserRequest::sendReqGetUser($idUser);
     }
     //Función para actualizar usuario
-    public static function UpdateUser($idUser, $rfc, $name, $unlimited){
-        $data = array(
-            'RFC' => $rfc,
-            'Name' => $name,
-            'Unlimited' => $unlimited
-        );
-        return AccountUserRequest::sendReqServicesUser($idUser, $data, "update",);
+    public static function UpdateUser($idUser, $data){
+        return AccountUserRequest::sendReqServicesUser($idUser, $data, "put");
     }
     //Función para eliminar usuario
     public static function DeleteUser($idUser){
