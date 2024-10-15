262d668c514f25f2c16a7a1031495607e4f59e90
Sebastian Vidal <sebastian.vidal@sw.com.mx>
2024-10-15 14:25:09 -0600

    Add function to generate original chain

--- SWServices/Retention/RetencionesRequest.php
+++ SWServices/Retention/RetencionesRequest.php
@@ -2,7 +2,7 @@
 
 namespace SWServices\Retention;
 
-use SWServices\Helpers\ResponseHelper as Response;
+use SWServices\Retention\RetencionesHelper as Response;
 use SimpleXMLElement;
 use Exception;
 
@@ -47,7 +47,7 @@ class RetencionesRequest
             return Response::handleSoapError($responseSoap);
         }
 
-        return self::processSoapResponse($responseSoap);
+        return self::processSoapResponse($responseSoap, $soapAction);
     }
 
     private static function createSoapRequest(string $xmlRetencion, string $tokenAutenticacion, string $version)
@@ -65,21 +65,21 @@ class RetencionesRequest
         </soapenv:Envelope>';
     }
 
-    private static function processSoapResponse(string $responseSoap)
+    private static function processSoapResponse(string $responseSoap, string $version)
     {
         $responseXml = simplexml_load_string($responseSoap);
         if ($responseXml === false) {
-            throw new Exception("Failed to parse XML response.");
+            return Response::handleSoapError($responseSoap);
         }
 
         $namespaces = $responseXml->getNamespaces(true);
         $body = $responseXml->children($namespaces['s'])->Body;
-        $decodedCfdi = $body->children($namespaces[''])->TimbrarRetencionXMLResponse->TimbrarRetencionXMLResult;
-        $cfdi = htmlspecialchars_decode($decodedCfdi);
 
+        $decodedCfdi = self::getDecodedCfdi($body, $namespaces, $version);
+
+        $cfdi = htmlspecialchars_decode($decodedCfdi);
         $tfdDecoded = self::loadXmlWithNamespace($cfdi, self::XMLNS_TFD);
         $retentionDecoded = self::loadXmlWithNamespace($cfdi, self::XMLNS_RETENTION);
-
         return json_encode(self::buildResponseData($tfdDecoded, $retentionDecoded, $cfdi), JSON_PRETTY_PRINT);
     }
 
@@ -90,21 +90,60 @@ class RetencionesRequest
         return $decoded;
     }
 
+    private static function getDecodedCfdi(SimpleXMLElement $body, array $namespaces, string $version): string
+    {
+        if ($version === 'v1') {
+            return (string) $body->children($namespaces[''])->TimbrarRetencionXMLResponse->TimbrarRetencionXMLResult;
+        } else {
+            return (string) $body->children($namespaces[''])->TimbrarRetencionXMLV2Response->TimbrarRetencionXMLV2Result;
+        }
+    }
+
     private static function buildResponseData(SimpleXMLElement $tfd, SimpleXMLElement $retenciones, string $cfdi)
     {
+
+        $tfdNode = $tfd->xpath('//tfd:TimbreFiscalDigital')[0] ?? null;
+        $retencionesNode = $retenciones->xpath('//retenciones:Retenciones')[0] ?? null;
+
+        if ($tfdNode === null || $retencionesNode === null) {
+            throw new Exception("Nodo TFD o Retenciones no encontrado en el CFDI.");
+        }
+        $cadenaOriginal = self::generateOriginalChain($tfd);
+
         return [
             'data' => [
-                'cadenaOriginalSAT' => '',
-                'noCertificadoSAT' => (string)$tfd->xpath('//tfd:TimbreFiscalDigital')[0]['NoCertificadoSAT'],
-                'noCertificadoCFDI' => (string)$retenciones->xpath('//retenciones:Retenciones')[0]['NoCertificado'],
-                'uuid' => (string)$tfd->xpath('//tfd:TimbreFiscalDigital')[0]['UUID'],
-                'selloSAT' => (string)$tfd->xpath('//tfd:TimbreFiscalDigital')[0]['SelloSAT'],
-                'selloCFDI' => (string)$tfd->xpath('//tfd:TimbreFiscalDigital')[0]['SelloCFD'] ?? (string)$retenciones->xpath('//retenciones:Retenciones')[0]['Sello'],
-                'fechaTimbrado' => (string)$tfd->xpath('//tfd:TimbreFiscalDigital')[0]['FechaTimbrado'],
+                'cadenaOriginalSAT' => $cadenaOriginal,
+                'noCertificadoSAT' => (string)$tfdNode['NoCertificadoSAT'],
+                'noCertificadoCFDI' => (string)$retencionesNode['NoCertificado'],
+                'uuid' => (string)$tfdNode['UUID'],
+                'selloSAT' => (string)$tfdNode['SelloSAT'],
+                'selloCFDI' => (string)($tfdNode['SelloCFD'] ?? $retencionesNode['Sello']),
+                'fechaTimbrado' => (string)$tfdNode['FechaTimbrado'],
                 'qrCode' => '',
                 'cfdi' => $cfdi,
             ],
             'status' => 'success',
         ];
+    }
+
+    private static function generateOriginalChain(SimpleXMLElement $tfd)
+    {
+        $tfdNode = $tfd->xpath('//tfd:TimbreFiscalDigital')[0] ?? null;
+
+        if ($tfdNode === null) {
+            return '';
+        }
+
+        $attributes = [
+            'version' => '||1.1',
+            'uuid' => '|' . (string)$tfdNode['UUID'],
+            'fechaTimbrado' => '|' . (string)$tfdNode['FechaTimbrado'],
+            'rfcProv' => '|' . (string)$tfdNode['RfcProvCertif'],
+            'selloCFD' => '|' . (string)$tfdNode['SelloCFD'],
+            'noCertSat' => '|' . (string)$tfdNode['NoCertificadoSAT'],
+        ];
+
+        $cadenaOriginal = implode('', $attributes);
+        return $cadenaOriginal;
     }
 }
