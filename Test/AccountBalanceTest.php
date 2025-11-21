24489aa9e6943f0b57fc3118a9ebb7ebd8870a2c
martinf12 <martin.flores@sw.com.mx>
2025-11-21 14:02:27 -0600

    Feature(SS-883): Add GetStampsById

--- Test/AccountBalanceTest.php
+++ Test/AccountBalanceTest.php
@@ -2,7 +2,7 @@
 namespace tests;
 use PHPUnit\Framework\TestCase;
 use SWServices\AccountBalance\AccountBalanceService as AccountBalanceService;
-final class AccountBalanceTests extends TestCase
+final class AccountBalanceTest extends TestCase
 {
     public function testSuccessGetBalanceByToken()
     {
@@ -115,6 +115,43 @@ final class AccountBalanceTests extends TestCase
         );
         $accountBalance = AccountBalanceService::Set($params);
         $result = $accountBalance::RemoveStamps("dec88317-e174-400a-9d23-9bb687444600", 1, "Prueba PHP Remove");
+        $this->assertEquals($resultSpect, $result->status);
+    }
+    public function testSuccessGetBalanceByUserIdByToken()
+    {
+        $resultSpect = "success";
+        $params = array(
+            "urlApi" => "https://api.test.sw.com.mx",
+            "token" => getenv('SDKTEST_TOKEN')
+        );
+        $accountBalance = AccountBalanceService::Set($params);
+        $result = $accountBalance::GetBalanceByUserId("fafb2ac2-62ca-49f8-91de-14cea73b01eb");
+        $this->assertEquals($resultSpect, $result->status);
+        $this->assertNotEmpty($result->data);
+    }
+    public function testSuccessGetBalanceByUserIdByAuth()
+    {
+        $resultSpect = "success";
+        $params = array(
+            "url"=>"https://services.test.sw.com.mx",
+            "urlApi" => "https://api.test.sw.com.mx",
+            "user" => getenv('SDKTEST_USER'),
+            "password" => getenv('SDKTEST_PASSWORD')
+        );
+        $accountBalance = AccountBalanceService::Set($params);
+        $result = $accountBalance::GetBalanceByUserId("fafb2ac2-62ca-49f8-91de-14cea73b01eb");
+        $this->assertEquals($resultSpect, $result->status);
+        $this->assertNotEmpty($result->data);
+    }
+    public function testErrorGetBalanceByUserId()
+    {
+        $resultSpect = "error";
+        $params = array(
+            "urlApi" => "https://api.test.sw.com.mx",
+            "token" => getenv('SDKTEST_TOKEN')
+        );
+        $accountBalance = AccountBalanceService::Set($params);
+        $result = $accountBalance::GetBalanceByUserId("00000000-0000-0000-0000-000000000000");
         $this->assertEquals($resultSpect, $result->status);
     }
 }
